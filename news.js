9ca302fa4c898137c8aeda47437f9c6dfdf7279d
rm53628rm <38837613+rm53628rm@users.noreply.github.com>
2025-12-19 23:40:40 +0530

    Update news.js

--- news.js
+++ news.js
@@ -20,7 +20,8 @@ function renderNews(containerId, limit = null){
       <h3>${n.t}</h3>
       <div class="news-meta">${n.d} | ${n.c}</div>
       <p>${n.p}</p>
-      <a href="archive.html" class="read-more">Read More</a>
+      <a href="news.html?id=${n.id}" class="read-more">Read More</a>
+      
     </div>`;
   });
 
