be862611d61f108896c25c27078e5ec3e10aace5
rm53628rm <38837613+rm53628rm@users.noreply.github.com>
2025-12-23 22:35:52 +0530

    Update latest.js

--- js/latest.js
+++ js/latest.js
@@ -1,1 +1,10 @@
+const latestNews=[
+{
+ title:"Market hits new high",
+ summary:"Sensex and Nifty touched new record today.",
+ date:"2025-01-20",
+ image:"https://via.placeholder.com/100",
+ link:"news/market-high.html"
+}
+];
 
