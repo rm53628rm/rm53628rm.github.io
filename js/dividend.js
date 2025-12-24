8e429f5b941f660d7b9a5c75b759fa6fb27e5dba
rm53628rm <38837613+rm53628rm@users.noreply.github.com>
2025-12-24 22:22:42 +0530

    Update dividend.js

--- js/dividend.js
+++ js/dividend.js
@@ -3,10 +3,9 @@ const dividendNews=[
  title:"Infosys Dividend Declared",
  summary:"Infosys announced interim dividend.",
  date:"2025-01-15",
+ category: "Dividend",
  image:"/images/market.jpg",
  link:"/news/market-high.html"
 }
 ];
-document.getElementById("categoryTitle").innerText =
-  cat.charAt(0).toUpperCase() + cat.slice(1) + " News";
 
