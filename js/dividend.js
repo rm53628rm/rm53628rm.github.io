ee81620b456e7c221ced749564818eae39c5de59
rm53628rm <38837613+rm53628rm@users.noreply.github.com>
2025-12-25 22:36:26 +0530

    Update dividend.js

--- js/dividend.js
+++ js/dividend.js
@@ -6,45 +6,5 @@ const dividendNews=[
  category: "Dividend",
  image:"/images/market.jpg",
  link:"/news/market-high.html"
-},
- {
-    title: "TCS Announces Final Dividend",
-    summary: "TCS declared a final dividend for FY 2024-25.",
-    date: "2025-02-02",
-    category: "Dividend",
-    image: "/images/tcs.jpg",
-    link: "/news/tcs-dividend.html"
- },
- {
-    title: "TCS Announces Final Dividend",
-    summary: "TCS declared a final dividend for FY 2024-25.",
-    date: "2025-02-02",
-    category: "Dividend",
-    image: "/images/tcs.jpg",
-    link: "/news/tcs-dividend.html"
- },
- {
-    title: "TCS Announces Final Dividend",
-    summary: "TCS declared a final dividend for FY 2024-25.",
-    date: "2025-02-02",
-    category: "Dividend",
-    image: "/images/tcs.jpg",
-    link: "/news/tcs-dividend.html"
- },
- {
-    title: "TCS Announces Final Dividend",
-    summary: "TCS declared a final dividend for FY 2024-25.",
-    date: "2025-02-02",
-    category: "Dividend",
-    image: "/images/tcs.jpg",
-    link: "/news/tcs-dividend.html"
- },
- {
-    title: "TCS Announces Final Dividend",
-    summary: "TCS declared a final dividend for FY 2024-25.",
-    date: "2025-02-02",
-    category: "Dividend",
-    image: "/images/tcs.jpg",
-    link: "/news/tcs-dividend.html"
- }
+}
 ];
