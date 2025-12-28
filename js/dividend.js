41ab5e25b4373ba59bb958ebca015c24ee2f9d26
rm53628rm <38837613+rm53628rm@users.noreply.github.com>
2025-12-28 09:07:56 +0530

    Update dividend.js

--- js/dividend.js
+++ js/dividend.js
@@ -8,7 +8,7 @@ dividendNews.push(
  date: "2025-12-25T22:00:00",
  category:"Dividend",
  image:"/images/market.jpg",
- link:"/news.html"
+ link:"/news.html?id=2"
 }
 );
 dividendNews.push(
@@ -19,6 +19,6 @@ dividendNews.push(
  date:"2025-01-15",
  category:"Dividend",
  image:"/images/infosys.jpg",
- link:"/news.html"
+ link:"/news.html?id=2"
 }
 );
