eac2f632bd9c07c655411695fc72da669d29d06b
rm53628rm <38837613+rm53628rm@users.noreply.github.com>
2025-12-30 21:06:05 +0530

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,23 @@
+// menu.js
+
+function openMenu(){
+  document.getElementById("mobileMenu").classList.add("show");
+}
+
+function closeMenu(){
+  document.getElementById("mobileMenu").classList.remove("show");
+}
+
+function toggleCat(){
+  const menu = document.getElementById("catMenu");
+  const arrow = document.getElementById("arrow");
+
+  if(menu.style.maxHeight){
+    menu.style.maxHeight = null;
+    arrow.innerText = "▼";
+  } else {
+    menu.style.maxHeight = menu.scrollHeight + "px";
+    arrow.innerText = "▲";
+  }
+}
 
